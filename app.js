7301363d0dc061d3dc1bfec28290b5ad1a446346
soomtong <soomtong@gmail.com>
2014-12-17 16:00:18 +0900

    `init` restify scaffold

--- app.js
+++ app.js
@@ -1,3 +1,59 @@
+var restify = require('restify');
+var socket = require('socket.io');
+
+var server = restify.createServer();
+var io = socket.listen(server);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+server.pre(restify.pre.sanitizePath());
+server.use(restify.requestLogger());
+server.use(restify.bodyParser({
+    maxBodySize: 0,
+    mapParams: true,
+    mapFiles: false,
+    overrideParams: false,
+    multipartHandler: function(part) {
+        part.on('data', function(data) {
+            console.log(data);
+        });
+    },
+    multipartFileHandler: function(part) {
+        part.on('data', function(data) {
+            console.log(data);
+        });
+    },
+    keepExtensions: false,
+    uploadDir: './'
+}));
+
+server.get('/:name', function respond(req, res) {
+        res.send('hello ' + req.params.name);
+    }
+);
+
+server.get('/note/:name', function respond(req, res) {
+        res.send('hello ' + req.params.name);
+    }
+);
+
+server.get(/.*/, restify.serveStatic({
+    directory: __dirname + '/client',
+    default: 'index.html'
+}));
+
+
+server.listen(3032, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
+/*
 var express = require('express');
 var app = express();
 
@@ -33,3 +89,4 @@ app.get('/:article_id?/setup', restrict, function (req, res) {
 app.post('/:article_id?/setup', restrict, function (req, res) {
     // save data to local sqlite engine with orm
 });
+*/
