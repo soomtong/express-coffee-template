73df914179f668c83d804a78ea58bea83b05ab99
soomtong <soomtong@gmail.com>
2014-12-23 20:10:06 +0900

    `update` ember scaffold

--- public/script/app.js
+++ public/script/app.js
@@ -1,5 +1,71 @@
-window.Harookit = Ember.Application.create();
+
+window.Harookit = Ember.Application.create({});
+/*
+
+Harookit.ApplicationAdapter = DS.FixtureAdapter;
+
+Harookit.Article = DS.Model.extend({
+    name         : DS.attr(),
+    email        : DS.attr(),
+    bio          : DS.attr(),
+    avatarUrl    : DS.attr(),
+    creationDate : DS.attr()
+});
+
+Harookit.Article.FIXTURES = [{
+    id: 1,
+    name: 'Sponge Bob',
+    email: 'bob@sponge.com',
+    bio: 'Lorem ispum dolor sit amet in voluptate fugiat nulla pariatur.',
+    avatarUrl: 'http://jkneb.github.io/ember-crud/assets/images/avatars/sb.jpg',
+    creationDate: 'Mon, 26 Aug 2013 20:23:43 GMT'
+}, {
+    id: 2,
+    name: 'John David',
+    email: 'john@david.com',
+    bio: 'Lorem ispum dolor sit amet in voluptate fugiat nulla pariatur.',
+    avatarUrl: 'http://jkneb.github.io/ember-crud/assets/images/avatars/jk.jpg',
+    creationDate: 'Fri, 07 Aug 2013 10:10:10 GMT'
+}];
+*/
 
 Harookit.Router.map(function() {
-    this.resource('articles', { path: '/' });
+    this.route("masthead", { path: "/head" });
+    this.route("articles", { path: "/articles" });
+    //this.resource('article', function() {
+    //    this.route('list', { path: ':slug' });
+    //});
 });
+
+Harookit.IndexRoute = Ember.Route.extend({
+    setupController: function(controller) {
+        // Set the IndexController's `title`
+        controller.set('title', "Harookit");
+        controller.set('version', "0.0.1");
+    }
+});
+
+Harookit.isLoginController = Ember.Controller.extend({
+    isLogin: false
+});
+
+Harookit.articlesRoute = Ember.Route.extend({
+    setupController: function(controller, isLogin) {
+        controller.set('model', isLogin);
+    }
+});
+
+/*
+Harookit.ArticlesRoute = Ember.Route.extend({
+    model: function() {
+        var articleObejcts = [];
+        Ember.$.getJSON('/articles', function(artists) {
+            artists.forEach(function(data) {
+                //articleObejcts.pushObject(Harookit.Article.createRecord(data));
+                articleObejcts.pushObject(Harookit.Article.createRecord(data));
+            });
+        });
+        return articleObejcts;
+    }
+});
+*/
